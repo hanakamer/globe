fd4451a6b38b747c31a17b0813ddb70df16e2520
hanakamer <hanakamer@gmail.com>
2016-02-08 21:27:44 +0200

    gradient circles added

--- scripts/plugins/drawCircle.js
+++ scripts/plugins/drawCircle.js
@@ -2,17 +2,22 @@ planetaryjs.plugins.circles = function(config) {
   var circle, center;
   var lat;
   var long;
+  var innerCircles;
 
-  var addCircle = function(_long, _lat){
+  var addCircle = function(_long, _lat, angle){
     lat = _lat;
     long = _long;
     circle = d3.geo.circle().origin([_long, _lat]).angle(0.4)();
-    center = d3.geo.circle().origin([_long, _lat]).angle(0.1)();
-
+    center = d3.geo.circle().origin([_long, _lat]).angle(0.2)();
+    innerCircles = [];
   }
 
   var setRadius = function(angle){
     circle = d3.geo.circle().origin([long, lat]).angle(angle)();
+    innerCircles = [];
+    for (var i = 0.1; i<angle; i+=0.7){
+      innerCircles.push(d3.geo.circle().origin([long, lat]).angle(i)());
+    }
   }
 
   return function (planet) {
@@ -24,6 +29,15 @@ planetaryjs.plugins.circles = function(config) {
     planet.onDraw(function() {
       if (!circle) return false;
       planet.withSavedContext(function(ctx) {
+
+        for (var obj in innerCircles){
+          ctx.beginPath();
+          ctx.strokeStyle =colors[colors.indexOf(mgn_color)-obj];
+          ctx.globalAlpha = 1;
+          planet.path.context(ctx)(innerCircles[obj]);
+          ctx.stroke();
+        }
+
         ctx.beginPath();
         ctx.fillStyle = mgn_color;
         ctx.globalAlpha = 0.5;
